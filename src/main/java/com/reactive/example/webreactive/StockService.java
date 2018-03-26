01f1d3bb2d1d7857696aad36e4270b2622baaf87
Sainath <sainathreddynarahari6@gmail.com>
2018-03-26 16:57:06 -0500

    Working Reactive Example for Multipe(Array) Stock Quotes

--- src/main/java/com/reactive/example/webreactive/StockService.java
+++ src/main/java/com/reactive/example/webreactive/StockService.java
@@ -5,26 +5,48 @@ import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class StockService {
 
+    private static String[] quotes = {"AAPL", "GOOG", "YHOO"};
 
     public Observable<Stock> getStock() {
         return Observable.create(
                 subscriber -> {
                     if (!subscriber.isUnsubscribed()) {
                         Stock stock = null;
-                        try {
-                            System.out.println("service:: Retrieve Stock Info.");
-                            stock = YahooFinance.get("GOOG");
-                            subscriber.onNext(stock);
-                        } catch (IOException e) {
-                            System.out.println(e);
-                            subscriber.onError(e);
-                        }
+
+                        Arrays.stream(quotes).map(quote -> {
+                            try {
+                                System.out.println("service:: Retrieve Stock Info -> " + quote);
+                                if (quote.equals("GOOG")) {
+                                    throw new IOException("GOOG Error");
+                                }
+                                return YahooFinance.get(quote);
+                            } catch (IOException e) {
+                                System.out.println(e);
+                                subscriber.onError(e);
+                            }
+                            return null;
+                        }).filter(stockInfo -> stockInfo != null)
+                                .forEach(stockInfo -> {
+                                    subscriber.onNext(stockInfo);
+                                    sleep(2000);
+                                    subscriber.onError(new RuntimeException("Exception Here!"));
+                                });
                     }
                     subscriber.onCompleted();
                 }
         );
+    }
+
+    private void sleep(int i) {
+        try {
+            System.out.println("sleep:: Sleeping for 2 Seconds");
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
